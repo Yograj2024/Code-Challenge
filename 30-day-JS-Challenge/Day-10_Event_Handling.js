f94cced613e7c89bab5b91096d2242a9c0781e01
Yograj Singh <ryog7910@gmail.com>
2024-07-25 06:21:09 -0800

    Completed Day-10

---
+++ 30-day-JS-Challenge/Day-10_Event_Handling.js
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Day-10 Event Handling</title>
+    <style>
+        *{
+            margin:0 0 0 10px;
+        }
+        .div{
+            margin: 20px;
+        }
+        .input{
+            margin: 0 0 10px 20px;
+        }
+        #input4{
+            margin: 0 0 10px 8px;
+        }
+        .bg_change2{
+            height: 100px;
+            width: 200px;
+            background-color: red;
+        }
+        .bg_change{
+            height: 100px;
+            width: 200px;
+            background-color:yellow;
+        }
+        ul>li:hover{
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+  
+    <div class="div" id="div1">
+        <p id="para1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores laboriosam culpa voluptatibus corporis adipisci eius laborum ipsum dicta sed ad eveniet aperiam hic placeat sit esse facere itaque optio veniam, voluptas aspernatur assumenda iure dolore odio? Reiciendis enim sed, explicabo obcaecati rerum sunt molestias repudiandae commodi maxime in ipsam. Doloribus?</p>
+        <button id="btn1">click me</button>
+    </div> <br>
+
+    <img src="./images.jfif" alt="img" id="image"><br>
+
+    <div class="div bg_change" id="div2"></div><br>
+
+    <input type="text"  class="input" id="input1"><br><br>
+    <input type="text"  class="input" id="input2"><br><br>
+    <p id="para2"></p><br><br>
+
+    <form action="" id="use_data">
+        Name <input type="text"  class="input" id="input3" name="use_name"><br>
+        Contact <input type="text" class="input" id="input4" name="use_contact"><br>
+        Email <input type="email" class="input" id="input5" name="use_email"><br>
+        <button id="btn2">submit</button>
+    </form><br>
+
+    <select name="" id="dropdown">
+        <option value="0">Select Language</option
+        <option value="python">Python</option>
+        <option value="java">Java</option>
+        <option value="JavaScript">JavaScript</option>
+        <option value="c">C</option>
+        <option value="c++">C++</option>
+        <option value="rust">Rust</option>
+    </select><br><br>
+    <p id="para3">Selected Language : none</p><br><br>
+<div>
+    <ul id="list">
+        <li>1 list</li>
+        <li>2 list</li>
+        <li>3 list</li>
+        <li>4 list</li>
+        <li>5 list</li>
+    </ul><br>
+    <button id="btn3">Add new </button>
+</div><br><br>
+<script>
+
+    // Activity 1:- Basic Event Handling.....
+    // Task 1: click event  -
+    let click=false;
+    let text=document.getElementById("para1").innerText;
+    document.getElementById("btn1").addEventListener("click",()=>{
+        if(!click){
+            document.getElementById("para1").innerText="hello";
+            click=true;
+        }else{
+            document.getElementById("para1").innerText=text;
+            click=false;
+        }
+    });
+
+    // Task 2: duble-click Event....
+    let click2=true;
+    let img=document.getElementById("image");
+    img.addEventListener("dblclick",()=>{
+        if(click2){
+            img.setAttribute("src","./imagesss.jfif");
+            // img.style.display='none';
+            click2=false;
+        }else{
+            img.setAttribute("src","./images.jfif");
+            click2=true;
+        }
+    });
+
+
+    // Activity 2:- Mouse Event 
+    // Task 3: mouseover  -
+    let div=document.getElementById("div2");
+    div.addEventListener("mouseover",()=>{
+        let bgColor=div.classList.contains("bg_change");
+        // div.style.backgroundColor="red"
+        if(bgColor){
+            div.classList.remove("bg_change");
+            div.classList.add("bg_change2");
+        };
+    });
+
+    // Task 4: mouseout  -
+    div.addEventListener("mouseout",()=>{
+        let bgColor=div.classList.contains("bg_change2");
+        // div.style.backgroundColor="green"
+        if(bgColor){
+            div.classList.remove("bg_change2");
+            div.classList.add("bg_change");
+        };
+    });
+
+
+    // Activity 3:- Keyboard Event......
+    // Task 5: Keypress -
+    let count=0;
+    document.getElementById("input1").addEventListener("keypress",(event)=>{
+        console.log(event.key);
+        count++;
+        if(event.key=="Enter"){
+            console.log(count);
+        }
+    });
+    
+    // Task 6: Keydown  -
+    let para=document.getElementById("para2");
+    let input=document.getElementById("input2");
+    document.getElementById("input2").addEventListener("keydown",()=>{
+        para.innerText=input.value;
+    });
+
+
+    // Activity 4:- Form Event.....
+    // Task 7: Submit event
+    let form=document.getElementById("use_data");
+    form.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        const Data=new FormData(form)
+        Data.forEach((value,key)=>{
+            console.log(`${key}: ${value}`);
+        });
+    }); 
+
+    // Task 8: change Event  -
+    let option=document.getElementById("dropdown");
+    let displayOption=document.getElementById("para3");
+    let prev_val=displayOption.innerText;
+    option.addEventListener("change",()=>{
+        if(option.value==0){
+            displayOption.innerText=prev_val;
+        }else{
+            displayOption.innerText=` ${prev_val.substring(0,20)} ${option.value}`;
+        };
+    });
+
+
+    // Activity 5:- Event Delegation.....
+    // Task 9:
+    let list=document.getElementById("list");
+    list.addEventListener("click",(e)=>{
+        console.log(`click content ${e.target.textContent}`);
+    });
+    // Task 10:
+    document.getElementById("btn3").addEventListener("click",()=>{
+        let new2=document.createElement("li");
+        new2.textContent=`${list.childElementCount+1} list`;
+        list.appendChild(new2);
+    })
+
+</script>
+</body>
+</html>
